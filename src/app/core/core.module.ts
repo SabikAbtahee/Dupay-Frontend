2b104e19734bcaec457b567b1732ad97a56c9538
sabikAbtahee <bsse0829@iit.du.ac.bd>
2019-09-30 16:30:33 +0600

    Change

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import {MutationService} from './mutation-services/mutation.service';
+import {QueryService} from './query-services/query.service';
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+	declarations: [],
+	imports: [ CommonModule ],
+	providers: [ MutationService, QueryService ]
 })
-export class CoreModule { }
+export class CoreModule {}
