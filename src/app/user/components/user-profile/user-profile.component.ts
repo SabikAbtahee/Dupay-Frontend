ce2ede6c685e5f522456c857ae81bae21b47870c
mohammad-ibrhaim <asdfiit15@gmail.com>
2019-10-24 09:11:41 +0600

    Profile form added

--- src/app/user/components/user-profile/user-profile.component.ts
+++ src/app/user/components/user-profile/user-profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {PasswordModalService} from "../../services/password-modal.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {UserProfileService} from "../../services/user-profile.service";
 
 @Component({
   selector: 'app-user-profile',
@@ -7,10 +9,36 @@ import {PasswordModalService} from "../../services/password-modal.service";
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
-
-  constructor(private passwordmodal: PasswordModalService,) { }
+  profileform: FormGroup;
+  user;
+  constructor(private passwordmodal: PasswordModalService,private fb: FormBuilder,
+              private userProfileService: UserProfileService
+              ) { }
 
   ngOnInit() {
+    this.makeProfileForm();
+    this.setProfileInformation();
+  }
+
+  setProfileInformation(){
+    this.user = this.userProfileService.getProfileInformation();
+  }
+  setProfileForm(user) {
+    this.profileform.controls.name.patchValue(user.name);
+    this.profileform.controls.phoneNumber.patchValue(user.phoneNumber);
+    // this.profileform.controls.role.patchValue(user.role);
+    this.profileform.controls.homeAddress.patchValue(user.homeAddress);
+    this.profileform.controls.shopAddress.patchValue(user.shopAddress);
+  }
+
+  makeProfileForm() {
+    this.profileform = this.fb.group({
+      name: ['', Validators.required],
+      phoneNumber: [''],
+      // role: [ '', Validators.required ],
+      homeAddress: [''],
+      shopAddress: ['']
+    });
   }
   openChangePasswordModal() {
     this.passwordmodal.openPasswordChangeModal();
