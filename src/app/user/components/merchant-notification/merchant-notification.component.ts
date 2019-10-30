06938dae810b7f7f3cfd22e1ba521da4a94f74af
AfiaSajeeda832 <bsse0832@iit.du.ac.bd>
2019-10-30 18:02:57 +0600

    merchant notification added. modifications are necessary though

--- src/app/user/components/merchant-notification/merchant-notification.component.ts
+++ src/app/user/components/merchant-notification/merchant-notification.component.ts
@@ -13,3 +13,76 @@ export class MerchantNotificationComponent implements OnInit {
   }
 
 }
+
+export interface MerchantNotification {
+  date: string;
+  id: string;
+  message: string;
+  description: string;
+}
+
+const NOTIFICATION_DATA: MerchantNotification[] = [
+  {
+    date: '1',
+    id: '123456789',
+    message: 'dummy text',
+    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
+        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
+  }, {
+   date: '1',
+    id: '123456789',
+    message: 'dummy text',
+    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
+        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
+  }, {
+    date: '1',
+    id: '123456789',
+    message: 'dummy text',
+    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
+        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
+  }, {
+    date: '1',
+    id: '123456789',
+    message: 'dummy text',
+    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
+        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
+  }, {
+    date: '1',
+    id: '123456789',
+    message: 'dummy text',
+    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
+        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
+  }, {
+     date: '1',
+    id: '123456789',
+    message: 'dummy text',
+    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
+        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
+  }, {
+   date: '1',
+    id: '123456789',
+    message: 'dummy text',
+    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
+        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
+  }, {
+    date: '1',
+    id: '123456789',
+    message: 'dummy text',
+    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
+        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
+  }, {
+    date: '1',
+    id: '123456789',
+    message: 'dummy text',
+    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
+        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
+  }, {
+  
+    date: '1',
+    id: '123456789',
+    message: 'dummy text',
+    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
+        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
+  }
+
+];
