eed454bd857ea7a798d00630dfa8d1c43e5b32ab
AfiaSajeeda832 <bsse0832@iit.du.ac.bd>
2019-11-02 15:18:31 +0600

    Informarl check on merchant notification

--- src/app/user/components/merchant-notification/merchant-notification.component.ts
+++ src/app/user/components/merchant-notification/merchant-notification.component.ts
@@ -1,12 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {trigger,state,style,animate,transition}from '@angular/animations';
 
 @Component({
   selector: 'app-merchant-notification',
   templateUrl: './merchant-notification.component.html',
-  styleUrls: ['./merchant-notification.component.scss']
+  styleUrls: ['./merchant-notification.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
+  
 })
 export class MerchantNotificationComponent implements OnInit {
 
+  dataSource = new MatTableDataSource(NOTIFICATION_DATA);
+  columnsToDisplay = ['date', 'id', 'message'];
+  expandedElement: MerchantNotification | null;
+
+  
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
   constructor() { }
 
   ngOnInit() {
@@ -19,70 +39,71 @@ export interface MerchantNotification {
   id: string;
   message: string;
   description: string;
+  isRead: boolean;
 }
 
 const NOTIFICATION_DATA: MerchantNotification[] = [
   {
     date: '1',
-    id: '123456789',
-    message: 'dummy text',
-    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
-        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
+    id: 'abc',
+    message: 'Dummy transaction of 5.00 BDT',
+    description:`At 10 pm GMT+6, transaction of 5 BDT took place`,
+    isRead:true
   }, {
-   date: '1',
-    id: '123456789',
-    message: 'dummy text',
-    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
-        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
+   date: '2',
+    id: 'bcd',
+    message: 'Dummy transaction of 5.00 BDT',
+    description: `At 10 pm GMT+6, transaction of 5 BDT took place`,
+    isRead:true
   }, {
-    date: '1',
-    id: '123456789',
-    message: 'dummy text',
-    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
-        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
+    date: '3',
+    id: 'cde',
+    message: 'Dummy transaction of 5.00 BDT',
+    description:`At 10 pm GMT+6, transaction of 5 BDT took place`,
+    isRead:true
+      }, {
+    date: '4',
+    id: 'def',
+    message: 'Dummy transaction of 5.00 BDT',
+    description:`At 10 pm GMT+6, transaction of 5 BDT took place`,
+    isRead:true
   }, {
-    date: '1',
-    id: '123456789',
-    message: 'dummy text',
-    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
-        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
+    date: '5',
+    id: 'efg',
+    message: 'Dummy transaction of 5.00 BDT',
+    description:`At 10 pm GMT+6, transaction of 5 BDT took place`,
+    isRead:true
+      }, {
+     date: '6',
+    id: 'fgh',
+    message: 'Dummy transaction of 5.00 BDT',
+    description:`At 10 pm GMT+6, transaction of 5 BDT took place`
+    ,isRead:true
   }, {
-    date: '1',
-    id: '123456789',
-    message: 'dummy text',
-    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
-        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
-  }, {
-     date: '1',
-    id: '123456789',
-    message: 'dummy text',
-    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
-        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
-  }, {
-   date: '1',
-    id: '123456789',
-    message: 'dummy text',
-    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
-        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
-  }, {
-    date: '1',
-    id: '123456789',
-    message: 'dummy text',
-    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
-        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
+   date: '7',
+    id: 'ghi',
+    message: 'Dummy transaction of 5.00 BDT',
+    description:`At 10 pm GMT+6, transaction of 5 BDT took place`,
+        isRead:true
   }, {
     date: '1',
     id: '123456789',
-    message: 'dummy text',
-    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
-        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
+    message: 'Dummy transaction of 5.00 BDT',
+    description:`At 10 pm GMT+6, transaction of 5 BDT took place`
+    ,isRead:true
   }, {
+    date: '1',
+    id: '123456789',
+    message: 'Dummy transaction of 5.00 BDT',
+    description:`At 10 pm GMT+6, transaction of 5 BDT took place`
+  ,isRead:true
+      }, {
   
     date: '1',
     id: '123456789',
-    message: 'dummy text',
-    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
-        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
-  }
+    message: 'Dummy transaction of 5.00 BDT',
+    description:`At 10 pm GMT+6, transaction of 5 BDT took place`
+        ,isRead:true
+      }
 
 ];
