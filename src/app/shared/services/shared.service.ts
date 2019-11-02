d73957a9f71d9d9dbed59095652973fca67dd57f
sabikAbtahee <bsse0829@iit.du.ac.bd>
2019-11-02 15:43:09 +0600

    Spinner wait screen added

--- src/app/shared/services/shared.service.ts
+++ src/app/shared/services/shared.service.ts
@@ -3,13 +3,14 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatDialog } from '@angular/material/dialog';
 import { SnackbarComponent } from '../components/snackbar/snackbar.component';
 import { snackbar } from '../../config/interfaces/configurations.interface';
+import { NgxSpinnerService } from "ngx-spinner";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
 
-  constructor(private snackbar: MatSnackBar,public dialog: MatDialog) { }
+  constructor(private snackbar: MatSnackBar,public dialog: MatDialog,private spinner: NgxSpinnerService) { }
 
   openSnackBar(configuration: snackbar) {
 		this.snackbar.openFromComponent(SnackbarComponent, {
@@ -19,5 +20,12 @@ export class SharedService {
 			verticalPosition: configuration.verticalPosition ? configuration.verticalPosition : 'bottom',
 			panelClass: configuration.panelClass ? configuration.panelClass : null
 		});
+	}
+
+	openSpinner(){
+		this.spinner.show();
+	}
+	hideSpinner(){
+		this.spinner.hide();
 	}
 }
