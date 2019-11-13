e87518564178df7e57c61df58fec42259c0cf51a
sabikAbtahee <bsse0829@iit.du.ac.bd>
2019-11-13 15:12:59 +0600

    Bug fixing sabik

--- src/app/config/constants/dupayConstants.ts
+++ src/app/config/constants/dupayConstants.ts
@@ -190,7 +190,7 @@ export const dupayConst = {
 };
 // export const passwordRegex = '^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$';
 export const passwordRegex = '.{8,}$';
-
+export const integerRegex='/^\d+$/';
 
 export const authentication_error_messages = {
   merchant_type: 'Please choose merchant type',
@@ -230,7 +230,8 @@ export const snackbarMessages = {
   change_password_fail: 'Password change has been failed',
   selected_merchant_notification_sent_success: 'Notification has been sent',
   selected_merchant_notification_sent_fail: 'Notification sent has been failed',
-  withdraw_success:'Request sent successfully'
+  withdraw_success:'Request sent successfully',
+  not_allowed:'Not allowed'
 };
 
 export const withdrawErrorMessages={
@@ -311,3 +312,7 @@ export const localStorageKeys = {
 
 }
 
+
+export const fileSize={
+  size:2000000
+}
