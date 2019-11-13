d38076cd816673455d60b7954cfce1d104be0c50
Ibrahim Khalil <ibra804him@gmail.com>
2019-11-13 14:43:52 +0600

    Refactored

--- src/app/transaction/components/history-transaction/history-transaction.component.ts
+++ src/app/transaction/components/history-transaction/history-transaction.component.ts
@@ -2,6 +2,7 @@ import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 import { ViewChild, Component, OnInit } from '@angular/core';
 import { ExportService } from '../../services/export.service';
 import { TransactionService } from '../../services/transaction.service';
+import { Transaction } from 'src/app/config/interfaces/dupay.interface';
 
 @Component({
   // tslint:disable-next-line: component-selector
@@ -12,24 +13,38 @@ import { TransactionService } from '../../services/transaction.service';
 export class HistoryTransactionComponent implements OnInit {
 
 
-  constructor(private exportService: ExportService,
-              private transactionService: TransactionService) { }
-  listData: MatTableDataSource<any>;
-  displayedColumns: string[] = ['SI No','id', 'time', 'amount'];
-  @ViewChild(MatSort, {static: false}  ) sort: MatSort;
-  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
+  constructor(private exportService: ExportService, private transactionService: TransactionService) { }
+
+  listData = new MatTableDataSource<Transaction>();
+  displayedColumns: string[] = ['SI No','transactionId', 'time', 'amount'];
+
+  @ViewChild(MatSort, {static: true}  ) sort: MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   searchKey: string;
 
 
   ngOnInit() {
-    this.transactionService.getAllTransactionsByUserId().subscribe(list => {
-         this.listData = new MatTableDataSource(list.content);
-        //  console.log('IBRAHIM' , this.listData);
-         this.listData.sort = this.sort;
-         this.listData.paginator = this.paginator;
-    });
-
+    this.setDataSource();
   }
+
+  setDataSource(){
+    let responseData = [];
+    this.transactionService.getAllTransactionsByUserId().subscribe(list => {
+      for(let i of list.content) {
+        responseData.push({
+          transactionId: i.transactionId,
+          time: i.payDate,
+          amount: i.payAmount,
+        });
+      }
+      // this.listData = new MatTableDataSource(list.content);
+     //  console.log('IBRAHIM' , this.listData);
+      this.listData.data = responseData;
+      this.listData.sort = this.sort;
+      this.listData.paginator = this.paginator;
+    });
+  }
+
   applyFilter(filterValue: string) {
     this.listData.filter = filterValue.trim().toLowerCase();
   }
